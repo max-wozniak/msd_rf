55487f787e71bdc340d61bcc23717a8727b7ac08
TylerTalarico <ttt8000@g.rit.edu>
2024-09-29 14:14:52 -0400

    MATLAB script for training data generation

---
+++ CNN/Modulations.m
@@ -0,0 +1,27 @@
+NUM_SIGNALS = 1000;
+mods = ["fm" "am"];
+NUM_MODS = length(mods);
+
+fs = 1000;
+dt = 1/fs;
+t = -0.5:dt:(0.5 - dt);
+
+D = zeros(NUM_SIGNALS*NUM_MODS, length(t) + 1);
+size(D)
+
+for mod = 1:NUM_MODS
+    for i = 1:NUM_SIGNALS
+        w = 2*pi*(25*rand(1)+10);
+        fc = (93*rand(1)+11);
+    
+        m = sin(w*t) + randn(size(t))/6;
+        u = [modulate(m, fc, fs, mods(mod)) mod];
+    
+        D(NUM_MODS*mod + i, :) = u;
+    end
+end
+
+writematrix(D, "TrainData.csv")
+
+
+
