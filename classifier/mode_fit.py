a56b3fe686ea21c369d5db248d52271141d9f139
Tyler Talarico <ttt8000@g.rit.edu>
2024-11-04 16:06:02 -0500

    Visualize Signals

--- classifier/mode_fit.py
+++ classifier/mode_fit.py
@@ -9,7 +9,7 @@ SAMPLES_PER_SPECTRUM = 512
 AVERAGE_ORDER = 12
 NUM_FEATURES = 4
 KERNEL_ORDER = 1
-TEST_INDEX = -1
+TEST_INDEX = 34
 
 BW_THRESH = 23.0
 
@@ -37,7 +37,8 @@ for infile in infiles:
     raw_gain[bad_indices] = 0
     
     # Sliding window average to smooth the FFT
-    raw_gain = raw_gain.reshape( (SAMPLES_PER_SPECTRUM, -1) ).T
+    raw_gain = raw_gain.reshape( (-1, SAMPLES_PER_SPECTRUM) )
+    print(raw_gain.shape)
     average_filter = np.ones((AVERAGE_ORDER,), dtype=float)/AVERAGE_ORDER
     avg_gain = np.zeros((raw_gain.shape[0], raw_gain.shape[1] - average_filter.shape[0] + 1))
     
@@ -56,6 +57,7 @@ for infile in infiles:
     fmin = 0
     fmax = avg_gain.shape[1]-1    
     for i in range(avg_gain.shape[0]):
+        '''
         if avg_gain[i][0] < BW_THRESH:
             for j in range(avg_gain.shape[1]-1):
                 if avg_gain[i][j] < BW_THRESH and avg_gain[i][j+1] > BW_THRESH:
@@ -66,10 +68,12 @@ for infile in infiles:
                 if avg_gain[i][j] < BW_THRESH and avg_gain[i][j-1] > BW_THRESH:
                     fmax = j
                     break
-        train_data_tmp[i, 0] = fmax-fmin
-        train_data_tmp[i, 1] = np.var(avg_gain[i, fmin+20:fmax+1-20])
-        train_data_tmp[i, 2] = np.mean(avg_gain[i, fmin+20:fmax+1-20])
-        train_data_tmp[i, 3] = np.max(dG[i, :])
+        '''
+        # train_data_tmp[i, 0] = fmax-fmin
+        train_data_tmp[i, 0] = np.var(avg_gain[i, :])
+        train_data_tmp[i, 1] = np.mean(avg_gain[i, :])
+        train_data_tmp[i, 2] = np.var(dG[i, :])
+        train_data_tmp[i, 3] = np.mean(dG[i, :])
      
         
     # Assign labels
@@ -94,24 +98,45 @@ for infile in infiles:
         test_labels = np.concatenate((test_labels, ytest))
     
             
-'''
+
 print('Fmin: ', fmin)
 print('Fmax: ', fmax)
 print('Width: ', fmax-fmin)
-print('Signal Std: ', np.var(avg_gain[-1, fmin+20:fmax+1-20]))
-print('Signal Avg: ', np.mean(avg_gain[-1, fmin+20:fmax+1-20]))
-'''
+print('Signal Var: ', np.var(avg_gain[TEST_INDEX, fmin+20:fmax+1-20]))
+print('Signal Avg: ', np.mean(avg_gain[TEST_INDEX, fmin+20:fmax+1-20]))
+print('Diff Avg: ', np.mean(dG[TEST_INDEX, fmin+20:fmax+1-20]))
+print('Diff Var: ', np.var(dG[TEST_INDEX, fmin+20:fmax+1-20]))
+
 
 train_labels = np.ravel(train_labels)
 test_labels = np.ravel(test_labels)
 
-plt.plot(avg_gain[TEST_INDEX,:])
+plt.plot(avg_gain[TEST_INDEX, :])
+#plt.plot(raw_gain[TEST_INDEX, :])
 plt.plot(dG[TEST_INDEX, :])
+plt.xlabel('Frequency Index')
+plt.ylabel('FFT Magnitude')
+plt.legend(['Smoothed Signal', 'FFT(i) - FFT(i-1)'], loc='upper left')
+
+print(train_data.shape)
+print(test_data.shape)
+fig = plt.figure()
+ax = fig.add_subplot(projection='3d')
+pos_mask = train_labels == 1
+neg_mask = train_labels != 1
+positive = train_data[pos_mask, :]
+negative = train_data[neg_mask, :]
+ax.scatter(positive[:, 1], positive[:, 2], positive[:, 3], marker='o')
+ax.scatter(negative[:, 1], negative[:, 2], negative[:, 3], marker='^')
+ax.set_xlabel('Gain Variance')
+ax.set_ylabel('Gain Mean')
+ax.set_zlabel('Diff Variance')
 plt.show()
 
 # Fit the model
 model = svm.LinearSVC(dual=False, max_iter=10000)
 model.fit(train_data, train_labels)
+print('Score: ', model.score(test_data, test_labels))
 
 # Save the decision boundary parameters to a file
 polarity = np.sign(model.intercept_)[0] if model.predict(np.zeros((1, NUM_FEATURES))) == 1 else -np.sign(model.intercept_)[0]
---
+++ classifier/visualize.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from os import listdir
+from os.path import isfile, join
+
+SAMP_NUM = 50
+
+infile = '.\\train\\channel1_fm_pl.out'
+raw_gain = np.genfromtxt(infile, dtype=float)[:, 1]
+raw_gain = raw_gain[SAMP_NUM*512:(SAMP_NUM+1)*512]
+
+plt.plot(raw_gain)
+plt.show()
+
